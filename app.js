9cf72300cff0dbb1fa68d639702fa43b0940db2f
Otis Singfield <singfieldotis@gmail.com>
2021-04-13 22:03:27 -0400

    add answer validation to questions

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
     {
       type: "input",
       name: "name",
-      message: "What is your name?",
+      message: "What is your name? (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub Username",
+      message: "Enter your GitHub Username (Required)",
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      },
     },
     {
       type: "input",
@@ -45,12 +61,28 @@ const promptProject = (portfolioData) => {
       {
         type: "input",
         name: "name",
-        message: "What is the name of your project?",
+        message: "What is the name of your project? (Required)",
+        validate: projectNameInput => {
+          if (projectNameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        },
       },
       {
         type: "input",
         name: "description",
         message: "Provide a description of the projeect (Required)",
+        validate: projectDescInput => {
+          if (projectDescInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        },
       },
       {
         type: "checkbox",
@@ -70,6 +102,14 @@ const promptProject = (portfolioData) => {
         type: "input",
         name: "link",
         message: "Enter the GitHub link to your project. (Required)",
+        validate: projectLinkInput => {
+          if (projectLinkInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        },
       },
       {
         type: "confirm",
